e87ed3189e2ca7d70349dbacd007b66811ce58f8
BISHAL DATTA <dattabishal001@gmail.com>
2023-07-24 20:01:09 +0530

    vedio strimming
    this is a vedio striming app like youtube

---
+++ src/components/WatchPage.js
@@ -0,0 +1,29 @@
+import { closeMenu } from "../utils/appSlice";
+import { useSearchParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+const WatchPage = () => {
+  const [searchParams] = useSearchParams();
+  console.log(searchParams.get("v"));
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(closeMenu());
+  }, []);
+  return (
+    <div className="px-5">
+      <iframe
+        width="1200"
+        height="600"
+        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default WatchPage;
---
+++ src/utils/contants.js
@@ -0,0 +1,5 @@
+const GOOGLE_API_KEY = "AIzaSyBUyfe_YR_lBQEhNEBfpI5WPfj2zzSrWe0";
+
+export const YOUTUBE_VIDEOS_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  GOOGLE_API_KEY;
